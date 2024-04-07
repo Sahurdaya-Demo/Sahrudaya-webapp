d6e988147894e08073d744ccb2fd44b37a7ee4f0
Sayoo123 <sayoojsunil2000@gmail.com>
2024-04-07 06:12:28 -0700

    okay

--- project/src/components/Utils/Resource.jsx
+++ project/src/components/Utils/Resource.jsx
@@ -1,8 +1,4 @@
 
-// export const LinkApi='https://sayooj123.pythonanywhere.com/';
+export const LinkApi='https://sayooj123.pythonanywhere.com/';
 
-export const LinkApi='http://127.0.0.1:8000/';
-
-const { EventEmitter } = require("events");
-const eventEmitter = new EventEmitter();
-export default eventEmitter;
+// export const LinkApi='http://127.0.0.1:8000/';
